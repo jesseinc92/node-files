78427d3b636a385ec0becb7fc1f997bce68e35c0
Jesse Hill <jesse.o.hill@gmail.com>
2021-11-15 22:49:55 -0500

    implemented step 2

--- step1.js
+++ step1.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const process = require('process');
 
 function cat(path) {
 
---
+++ step2.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const process = require('process');
+const axios = require('axios');
+
+
+function cat(path) {
+
+    // read the file located at path
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            console.log('Error! ', err);
+            process.exit(1);
+        }
+        console.log(data);
+    });
+}
+
+
+async function webCat(path) {
+
+    // read the url located at path
+    let resp = await axios.get(path);
+    console.log(resp.data)
+}
+
+
+let path = process.argv[2];
+
+if (path.substr(0, 4) == 'http') {
+    webCat(path);
+} else {
+    cat(path);
+}
